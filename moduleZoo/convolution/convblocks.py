c20d1331d1e308fd007f82d3504aaad636986173
Shivam Pandey <pandeyshivam2017robotics@gmail.com>
2021-10-29 15:39:55 +0530

    InvertedResidual Block added.
    Signed-off-by: Shivam Pandey <pandeyshivam2017robotics@gmail.com>

---
+++ moduleZoo/convolution/convblocks.py
@@ -0,0 +1,70 @@
+'''
+Copyright (C) 2021  Shivam Pandey
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
+from typing import Any, Callable, List, Optional
+
+import torch.nn as nn
+
+
+class ConvNormActivation(nn.Sequential):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: int = 3,
+        stride: int = 1,
+        padding: Optional[int] = None,
+        groups: int = 1,
+        norm_layer: Optional[Callable[..., nn.Module]] = nn.BatchNorm2d,
+        activation_layer: Optional[Callable[..., nn.Module]] = nn.ReLU,
+        dilation: int = 1,
+        inplace: bool = True,
+    ) -> None:
+        """Typical Convolution Normalization and Activation stack for easier implementation
+
+        Args:
+            in_channels (int): Number of input channels
+            out_channels (int): Number of output channels
+            kernel_size (int, optional): Kernel size for convolution. Defaults to 3.
+            stride (int, optional): Stride value for convolution. Defaults to 1.
+            padding (Optional[int], optional): Padding value, (passed to nn.Conv2D). Defaults to None.
+            groups (int, optional): Number of groups input tensor will be divided, (passed to nn.Conv2D). Defaults to 1.
+            norm_layer (Optional[Callable[..., nn.Module]], optional): Normalization layer. Defaults to nn.BatchNorm2d.
+            activation_layer (Optional[Callable[..., nn.Module]], optional): Activation function. Defaults to nn.ReLU.
+            dilation (int, optional): Dilation used in convolution (passed in nn.Conv2D). Defaults to 1.
+            inplace (bool, optional): Whether to use inplace operations or not. Defaults to True.
+        """
+        if padding is None:
+            padding = (kernel_size - 1) // 2 * dilation
+        layers = [
+            nn.Conv2d(
+                in_channels,
+                out_channels,
+                kernel_size,
+                stride,
+                padding,
+                dilation=dilation,
+                groups=groups,
+                bias=norm_layer is None,
+            )
+        ]
+        if norm_layer is not None:
+            layers.append(norm_layer(out_channels))
+        if activation_layer is not None:
+            layers.append(activation_layer(inplace=inplace))
+        super().__init__(*layers)
+        self.out_channels = out_channels
---
+++ moduleZoo/resblocks/invertedresidual.py
@@ -0,0 +1,80 @@
+'''
+Copyright (C) 2021  Shivam Pandey
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
+from typing import Any, Callable, List, Optional
+
+import torch.nn as nn
+from torch import Tensor
+
+from ..convolution import ConvNormActivation
+
+
+class InvertedResidual(nn.Module):
+    def __init__(self,
+                 in_channels: int,
+                 out_channels: int,
+                 expansion_ratio: float,
+                 stride: int,
+                 norm_layer: Optional[Callable[..., nn.Module]] = None) -> None:
+        """Inverted Residual Block
+
+        Args:
+            in_channels (int): Number of input channels
+            out_channels (int): Number of output channels
+            expansion_ratio (float): Expantion ratio, determines the width of hidden layers
+            stride (int): stride for conv blocks, must be in [1, 2], if >1 then rsidual connection will also be skipped
+            norm_layer (Optional[Callable[..., nn.Module]], optional): normalization layer, applied just after convolition and before activation function. Defaults to None.
+        """
+        super().__init__()
+        assert stride in [1, 2]
+
+        stride = stride
+
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+
+        hidden_channels = round(in_channels * expansion_ratio)
+        self.res_connect = stride == 1 and in_channels == out_channels
+
+        layers: List[nn.Module] = []
+        if expansion_ratio != 1:
+            layers.append(ConvNormActivation(in_channels, hidden_channels, kernel_size=1,
+                                             stride=stride, norm_layer=norm_layer, activation_layer=nn.Relu6))
+        layers.extend(
+            [
+                ConvNormActivation(
+                    hidden_channels,
+                    hidden_channels,
+                    stride = stride,
+                    groups = hidden_channels,
+                    norm_layer = norm_layer,
+                    activation_layer = nn.ReLU6
+                ),
+
+                nn.Conv2d(hidden_channels, out_channels, 1, 1, 0, bias=False),
+                norm_layer(out_channels)
+            ]
+        )
+        self.conv = nn.Sequential(*layers)
+        self.out_channels = out_channels
+        self._is_cn = stride > 1
+
+    def forward(self, x: Tensor) -> Tensor:
+        if self.res_connect:
+            return x + self.conv(x)
+        else:
+            return self.conv(x)
